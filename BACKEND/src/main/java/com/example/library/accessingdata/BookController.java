97d0b8d2c2c830b4ddf50622b75ea94ed946345f
cyngiel <pstefanski07@gmail.com>
2022-04-25 23:06:44 +0200

    Database added to the repository Docker database added. Created testing REST GET /book/all & POST /book/add

---
+++ BACKEND/src/main/java/com/example/library/accessingdata/BookController.java
@@ -0,0 +1,38 @@
+package com.example.library.accessingdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller // This means that this class is a Controller
+@RequestMapping(path="/book")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody
+    String addNewUser (@RequestParam String title, @RequestParam String author, @RequestParam String category, @RequestParam int publish_year, @RequestParam String publishing_house, @RequestParam String description, @RequestParam String catalog_number, @RequestParam int items) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        Book b = new Book();
+        b.setTitle(title);
+        b.setAuthor(author);
+        b.setCategory(category);
+        b.setPublish_year(publish_year);
+        b.setDescription(description);
+        b.setPublishing_house(publishing_house);
+        b.setCatalog_number(catalog_number);
+        b.setItems(items);
+        b.setAvailable_items(items);
+        bookRepository.save(b);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Book> getAllBooks() {
+        // This returns a JSON or XML with the users
+        return bookRepository.findAll();
+    }
+}
---
+++ DATABASE/init_db.sql
@@ -0,0 +1,41 @@
+CREATE SCHEMA IF NOT EXISTS library;
+
+DROP TABLE IF EXISTS library.book;
+
+CREATE TABLE library.book
+(
+    title               VARCHAR(50) NOT NULL,
+    author              VARCHAR(50) NOT NULL,
+    category            VARCHAR(50) NOT NULL,
+    publish_year        INT(4) UNSIGNED NOT NULL,
+    publishing_house    VARCHAR(50), 
+    description         VARCHAR(1000), 
+    catalog_number      VARCHAR(20), 
+    items               INT(3) UNSIGNED NOT NULL, 
+    available_items     INT(3) UNSIGNED NOT NULL, 
+    book_id             INT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (book_id)
+);
+
+DROP TABLE IF EXISTS library.user;
+
+CREATE TABLE library.user
+(
+    name                VARCHAR(30) NOT NULL,
+    last_name           VARCHAR(30) NOT NULL,
+    email               VARCHAR(30) NOT NULL,
+    password            VARCHAR(30) NOT NULL,
+    user_id             INT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (user_id)
+);
+
+#########################################################
+
+insert into library.book (title, author, category, publish_year, publishing_house, catalog_number, items, available_items, description)
+values ("Harry Potter i Kamień Filozoficzny", "J.K. Rowling", "Fantasy", 2016, "Media Rodzina", "9788380084322", 5, 2, 
+    "Książka „Harry Potter i Kamień Filozoficzny” rozpoczyna cykl o młodym czarodzieju i jego licznych przygodach. Tytułowy Harry Potter wychowywany jest przez nieprzychylnych mu ciotkę i wuja. Jego rodzice zginęli w tajemniczych okolicznościach, a jedyne, co mu po nich pozostało to blizna na czole w kształcie błyskawicy. W dniu swoich 11. urodzin bohater dowiaduje się, że istnieje świat, o którym nie miał pojęcia.");
+
+insert into library.user (name, last_name, email, password)
+values ("Jan", "Kowalski", "jan.kowalski@example.com", "haslo123");
