833e239136ba8108d045a0b19c5840c51cdb45bc
cyngiel <pstefanski07@gmail.com>
2022-06-02 00:13:29 +0200

    book endpoints fixed

--- BACKEND/src/main/java/com/example/library/accessingdata/BookController.java
+++ BACKEND/src/main/java/com/example/library/accessingdata/BookController.java
@@ -28,40 +28,11 @@ public class BookController {
     public @ResponseBody
     ResponseEntity<String> addNewBook(@RequestBody Book book) {
         bookRepository.save(book);
-        newsRepository.save(new News(book.getTitle(), book.getAuthor()));
-        return new ResponseEntity<>("saved", HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/add/item")
-    public @ResponseBody
-    ResponseEntity<String> deleteBookItem(@RequestBody BookItem bookItem) {
-        bookItemRepository.save(bookItem);
-        return new ResponseEntity<>("saved", HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/delete/book")
-    public @ResponseBody
-    ResponseEntity<String> deleteBook(int bookId) {
-        if(!bookRepository.findById(bookId).isPresent()){
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        }
-        try {
-            bookRepository.deleteById(bookId);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/delete/item")
-    public @ResponseBody
-    ResponseEntity<String> addNewBookItem(int bookItemId) {
-        try {
-            bookItemRepository.deleteById(bookItemId);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
+        News news = new News();
+        news.setTitle("Nowa pozycja w Bibliotece: " + book.getTitle() + "!");
+        news.setContent("W zaspbach naszej Biblioteki jest już dostępna nowa książka: " + book.getTitle() + ", autor: " + book.getAuthor() + ". Zapraszamy do czytania :)");
+        newsRepository.save(news);
+        return new ResponseEntity<>("saved", HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/all")
@@ -106,5 +77,78 @@ public class BookController {
                 .borrowedItemsCount((int) book.getBookItems().stream().filter(bookItem -> bookItem.getStatus().equals(BORROWED)).count())
                 .reservedItemsCount((int) book.getBookItems().stream().filter(bookItem -> bookItem.getStatus().equals(RESERVED)).count())
                 .build(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/reserve")
+    public @ResponseBody
+    ResponseEntity<String> reserveBookById(@RequestParam int id) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (!book.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Optional<BookItem> optionalBookItem = book.get().getBookItems().stream().filter(item -> item.getStatus().equals(STOCK)).findFirst();
+        if (!optionalBookItem.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        BookItem bookItem = optionalBookItem.get();
+        bookItem.setStatus(RESERVED);
+        bookItemRepository.save(bookItem);
+        return new ResponseEntity<>("Updated", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/reserve/all")
+    public @ResponseBody
+    ResponseEntity<Iterable<BookItemExtended>> getAllReservedBooks() {
+        Iterable<BookItem> allBooks = bookItemRepository.findAll();
+        List<BookItemExtended> reservedBooks = StreamSupport
+                .stream(allBooks.spliterator(), false)
+                .filter(bookItem -> bookItem.getStatus().equals(RESERVED))
+                .map(this::mapReservedBooks)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(reservedBooks, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/borrow")
+    public @ResponseBody
+    ResponseEntity<String> borrowBookById(@RequestParam int id) {
+        Optional<BookItem> optionalBookItem = bookItemRepository.findById(id);
+        if (!optionalBookItem.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        BookItem bookItem = optionalBookItem.get();
+        bookItem.setStatus(BORROWED);
+        bookItemRepository.save(bookItem);
+        return new ResponseEntity<>("Updated", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/borrow/all")
+    public @ResponseBody
+    ResponseEntity<Iterable<BookItemExtended>> getAllBorrowedBooks() {
+        Iterable<BookItem> allBooks = bookItemRepository.findAll();
+        List<BookItemExtended> reservedBooks = StreamSupport
+                .stream(allBooks.spliterator(), false)
+                .filter(bookItem -> bookItem.getStatus().equals(BORROWED))
+                .map(this::mapReservedBooks)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(reservedBooks, HttpStatus.OK);
+    }
+
+    private BookItemExtended mapReservedBooks(BookItem bookItem) {
+        return new BookItemExtended.BookItemExtendedBuilder()
+                .book_id(bookItem.getBook().getBook_id())
+                .title(bookItem.getBook().getTitle())
+                .author(bookItem.getBook().getAuthor())
+                .category(bookItem.getBook().getCategory())
+                .items(bookItem.getBook().getItems())
+                .catalog_number(bookItem.getBook().getCatalog_number())
+                .publish_year(bookItem.getBook().getPublish_year())
+                .publishing_house(bookItem.getBook().getPublishing_house())
+                .book_item_id(bookItem.getBook_item_id())
+                .status(bookItem.getStatus().name())
+                .build();
     }
 }
