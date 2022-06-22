8e9a43255688e5a8948a7a48392f103af888a908
cyngiel <pstefanski07@gmail.com>
2022-06-22 11:43:21 +0200

    search fix

--- BACKEND/src/main/java/com/example/library/accessingdata/BookStats.java
+++ BACKEND/src/main/java/com/example/library/accessingdata/BookStats.java
@@ -1,7 +1,12 @@
 package com.example.library.accessingdata;
 
+import lombok.Data;
+
+@Data
 public class BookStats {
-    int stock, borrowed, reserved;
+    int stock;
+    int borrowed;
+    int reserved;
 
     public BookStats(int stock, int borrowed, int reserved) {
         this.stock = stock;
